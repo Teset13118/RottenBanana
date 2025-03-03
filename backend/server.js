2269ca717165b2c042889002677cc6d83a8f6d0b
Teset13118 <109953575+Teset13118@users.noreply.github.com>
2025-03-03 20:00:04 +0700

    fix

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,6 @@ const bodyParser = require('body-parser');
 require("dotenv").config();
 const app = express();
 
-// app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
